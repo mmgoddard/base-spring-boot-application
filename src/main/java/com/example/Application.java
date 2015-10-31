6150f320ed08cc8cc55cb6b16b95b7c4d2d83304
mmgoddard <mmgoddard25@hotmail.com>
2015-10-31 12:43:16 +0000

    Change: Updated pom.xml & properties file.

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,28 +1,17 @@
-/*
- * Copyright 2012-2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -33,4 +22,13 @@ public class Application extends SpringBootServletInitializer {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Component
+	static class Runner implements CommandLineRunner {
+
+		@Override
+		public void run(String... args) throws Exception {
+            LOGGER.info("Entering Command Line Runner .......");
+            //Test Code to go here
+		}
+	}
 }
